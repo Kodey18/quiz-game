8f6a905db449b3558039d2ab0c743f0ff8e6d4bf
karan adwani <karanadwani.official@gmail.com>
2023-09-12 23:32:17 +0530

    deploy

---
+++ js/questions.js
@@ -0,0 +1,67 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What are child legal rights?",
+    answer: "Laws that protect children",
+    options: [
+      "Parental privileges",
+      "Laws that protect children",
+      "School rules",
+      "Adult responsibilities",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which international document outlines the rights of children?",
+    answer: "United Nations Convention on the Rights of the Child (CRC)",
+    options: [
+      "Universal Declaration of Human Rights",
+      "United Nations Convention on the Rights of the Child (CRC)",
+      "Geneva Conventions",
+      "North Atlantic Treaty",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What age group does child legal rights typically apply to?",
+    answer: "0 to 18",
+    options: ["18 and above", "12 and above", "0 to 18", "6 and above"],
+  },
+  {
+    numb: 4,
+    question: "Which of the following is NOT a child's legal right?",
+    answer: "Right to vote",
+    options: [
+      "Right to protection from exploitation",
+      "Right to education",
+      "Right to vote",
+      "Right to healthcare",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does the term CRC stand for in child rights discussions?",
+    answer: "Children's Rights Convention",
+    options: [
+      "Child Rights Charter",
+      "Children's Rights Convention",
+      "Child Rights Code",
+      "Child Rights Committee",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
